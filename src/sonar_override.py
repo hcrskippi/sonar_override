c3b4d4957bd6d5240e3b3b72404c90906f372046
jcr09 <jcr09@ic.ac.uk>
2012-12-09 21:56:49 +0000

    Added GPS listener, need to add the name of the subscription

--- src/sonar_override.py
+++ src/sonar_override.py
@@ -12,7 +12,7 @@ pub= rospy.Publisher('cmd_vel',Twist)
 
 sonar_data = "0000"
 joystick_data = None
-
+multiplier = 1
 def sonarCallback(data):
     global sonar_data
     sonar_data = data.data
@@ -24,10 +24,22 @@ def joystickCallback(data):
     combinedCallback()
     #pub.publish(joystick_data)
 
+def gpsCallback(data);
+    global multiplier = 1;
+	if (data == 0) 
+		multiplier= 0
+	elif (data < 2)
+		multiplier = 0.25
+	elif (data < 5)
+		multiplier = 0.5 
+	elif (data <10) 
+		multiplier = 1
+	combinedCallback()
+
 def combinedCallback():
     global joystick_data
     global sonar_data
-    joystick_output = joystick_data
+    joystick_output = multiplier*joystick_data
     if ((sonar_data[0] == '1' or sonar_data[1] == '1') and joystick_data.linear.x > 0) or ((sonar_data[2] == '1' or sonar_data[3] == '1') and joystick_data.linear.x < 0):
         joystick_output.linear.x = 0
     pub.publish(joystick_output)
@@ -36,6 +48,7 @@ def main():
     rospy.init_node('sonarOverride', anonymous=False)
     rospy.Subscriber("sonar_step_detect", String, sonarCallback)
     rospy.Subscriber("joystick_cmd_vel", Twist, joystickCallback)
+    rospy.Subscriber("", Float64, gpsCallback)
     rospy.spin()
 
 if __name__ == '__main__':
